648254a320a87c525de9e71edf15381c2ed9780f
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2024-08-10 16:28:12 -0300

    Atualizar componentes

--- src/Server/ControllerList.php
+++ src/Server/ControllerList.php
@@ -2,7 +2,12 @@
 
 namespace Websyspro\Core\Server;
 
+use ReflectionAttribute;
 use Websyspro\Core\Common\Utils;
+use Websyspro\Core\Enums\ConstructStructure;
+use Websyspro\Core\Enums\Decoration;
+use Websyspro\Core\Enums\Method;
+use Websyspro\Core\Enums\MiddlewareStructure;
 
 class ControllerList
 {
@@ -22,14 +27,76 @@ class ControllerList
         ControllerItem::create(
           controllerRequest: $this->request,
           controllerResponse: $this->response,
-          controller: ServerUtils::getControllerName($controller),
-          controllerUrl: ServerUtils::GetControllerApi($controller),
-          controllerConstruct: ServerUtils::GetConstruct($controller),
-          controllerMiddlewares: ServerUtils::GetMiddlewares($controller)
+          controller: $this->getController($controller),
+          controllerUrl: $this->getControllerUrl($controller),
+          controllerConstruct: $this->getControllerConstruct($controller),
+          controllerMiddlewares: $this->getControllerMiddlewares($controller)
         )
       )
     ));
   }
+
+  public function getController(
+    string $controller
+  ): string {
+    return sprintf(
+      "%s{$controller}", DIRECTORY_SEPARATOR_WINDOWS
+    );
+  }
+
+  public function getControllerUrl(
+    string $controller
+  ): string {
+    return implode(
+      ServerUtils::GetApiBarSep(), [
+      ServerUtils::GetApiBase(),
+      ServerUtils::GetController(
+        $controller
+      )
+    ]);    
+  }
+  
+  public function getControllerConstruct(
+    string $controller
+  ): array {
+    $constrctFromController = Utils::Filter(get_class_methods($controller),
+      fn($method) => $method === Method::Contruct
+    );
+
+    $parametersFromContruct = Utils::Map($constrctFromController,
+      fn($method) => Reflect::getReflectMethod(
+        $controller, $method
+      )->getParameters()
+    );
+
+    $parametersFromContruct = Utils::Map($parametersFromContruct,
+      fn($parameters) => Utils::Map($parameters, 
+        fn($parameter) => DIRECTORY_SEPARATOR_WINDOWS . $parameter->getType()->getName()
+      )
+    );
+
+    return [
+      ConstructStructure::MethodName => Method::Contruct,
+      ConstructStructure::MethodParameters => Utils::ArrayFirtsValue(
+        $parametersFromContruct
+      )
+    ];
+  } 
+  
+  public function getControllerMiddlewares(
+    string $controller
+  ): array {
+    $MiddlewareFromController = Utils::Filter(Reflect::getAttributesFromReflectClass($controller),
+      fn(ReflectionAttribute $Attribute) => (
+        $Attribute->getName()::TypeDecoration === Decoration::Middleware
+      )
+    );
+
+    return Utils::Map($MiddlewareFromController, fn($attribute) => [
+      MiddlewareStructure::InstanceClass => DIRECTORY_SEPARATOR_WINDOWS . $attribute->getName(),
+      MiddlewareStructure::ArgsClass => $attribute->getArguments()
+    ]);
+  }  
 
   public function addController(
     mixed $controller
